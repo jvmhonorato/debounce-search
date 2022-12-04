154307790a007de7aa13948484040ee66e5b9371
vituhonorato <vituhonorato.89@gmail.com>
2022-12-04 20:34:35 -0300

    Add loadash.debounce

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import { useState } from 'react';
 import THANK_U_NEXT from './THANK_U_NEXT';
+import debounce  from 'lodash.debounce'
 
+
+//Main function
 const getFilteredItems =(query, items) => {
+  //if not be nothing typed show all items,
   if(!query){
     return items;
   }
+  // if you have something typed, filter by letter
   return items.filter((song)=> song.name.includes(query))
 }
 
@@ -18,8 +23,15 @@ function App() {
   const { items } = tracks
   
 
-  // is what is typed in search input, items is data come from API
+  // query: is what is typed in search input, items: is data come from API
   const filteredItems = getFilteredItems(query, items)
+
+  //instantiate the setQuery taking the typed values
+  const updateQuery = e =>setQuery(e?.target?.value)
+
+  
+ //join setQuery to fetch time of (denbounce)
+  const debounceOnChange = debounce(updateQuery,200)
   
 
 
@@ -27,7 +39,7 @@ function App() {
     <div className="App">
       <h1>Debounce Search</h1>
       <label>Search</label>
-      <input type="text" onChange={e => setQuery(e.target.value)}/>
+      <input type="text" onChange={debounceOnChange}/>
       <ul>
         {filteredItems.map((value) => ( 
         <h1 key={value.name}>{value.name}</h1> 
