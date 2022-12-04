8a6355d4d0d882d47cc71b0fbb481c32b752dfbd
vituhonorato <vituhonorato.89@gmail.com>
2022-12-04 19:54:32 -0300

    Create a dynamic search with debounce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import THANK_U_NEXT from './THANK_U_NEXT';
+
+const getFilteredItems =(query, items) => {
+  if(!query){
+    return items;
+  }
+  return items.filter((song)=> song.name.includes(query))
+}
+
+
 
 function App() {
+  const [query, setQuery] = useState('')
+  const { tracks } = THANK_U_NEXT
+  const { items } = tracks
+  
+
+  // is what is typed in search input, items is data come from API
+  const filteredItems = getFilteredItems(query, items)
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Debounce Search</h1>
+      <label>Search</label>
+      <input type="text" onChange={e => setQuery(e.target.value)}/>
+      <ul>
+        {filteredItems.map((value) => ( 
+        <h1 key={value.name}>{value.name}</h1> 
+        ))}
+      </ul>
     </div>
   );
 }
